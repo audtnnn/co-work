5c4abb74d3ac7b1606b573453f425e06453a8153
audtnnn <kaudtn12@gmail.com>
2024-03-26 16:18:17 +0900

    first commit

---
+++ src/views/EventView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <event-section />
+    </div>
+</template>
+
+<script>
+    import EventSection from '@/components/event/EventSection.vue'
+    export default {
+        name: "EventView",
+        components:{
+            EventSection,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/SurveyView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <survey-section />
+        <slider-section1 />
+        <slider-section2 />
+        <slider-section3 />
+        
+    </div>
+</template>
+
+<script>
+import SurveySection from '@/components/survey/SurveySection.vue';
+import SliderSection1 from '@/components/section/SliderSection1.vue';
+import SliderSection2 from '@/components/section/SliderSection2.vue';
+import SliderSection3 from '@/components/section/SliderSection3.vue';
+
+    export default {
+        components:{
+            SurveySection,
+            SliderSection1,
+            SliderSection2,
+            SliderSection3,
+           
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
